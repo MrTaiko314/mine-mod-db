234f8d7a74faf3af8d8d8495f32138715e65d008
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-04-19 23:38:41 -0300

    Add get_mods function

--- minemoddb/program.py
+++ minemoddb/program.py
@@ -5,7 +5,7 @@ import minemoddb.cli.screen as screen
 from minemoddb.models.person import Person
 from minemoddb.models.mod import Mod
 from minemoddb.models.modpack import Modpack
-from minemoddb.utils import clear_screen, get_number
+from minemoddb.utils import clear_screen, get_number, get_numbers
 
 
 class Program:
@@ -57,6 +57,19 @@ class Program:
         selected_number = get_number(
             prompt=prompt, min_value=1, max_value=len(entries))
         return sorted_mod_list[selected_number - 1]
+
+    def get_mods(self, prompt: str) -> list[Mod]:
+        entries = self._mod_database.entries
+        if len(entries) == 0:
+            raise RuntimeError('Mod database is empty')
+        print(f"Mods ({len(entries)}):")
+        sorted_mod_list = sorted(entries.values(), key=lambda x: x.name)
+        for i, mod in enumerate(sorted_mod_list, 1):
+            print(f"{i:3}. {mod.name} de {mod.owner.name}")
+        selected_numbers = get_numbers(
+            prompt=prompt, min_value=1, max_value=len(entries), separator=',')
+        mods = [sorted_mod_list[number-1] for number in selected_numbers]
+        return mods
 
     def get_modpack(self, prompt: str) -> Modpack:
         entries = self._modpack_database.entries
