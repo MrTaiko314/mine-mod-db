bbf4ee9125ac58fb0205e6e39ff31f837126b433
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-04-19 23:52:14 -0300

    Add modpack option on TablesMenuScreen

--- minemoddb/cli/tables_menu_screen.py
+++ minemoddb/cli/tables_menu_screen.py
@@ -2,6 +2,7 @@ from minemoddb.program import Program
 import minemoddb.cli.main_menu_screen as main_menu_screen
 import minemoddb.cli.person_table_menu_screen as person_table_menu_screen
 import minemoddb.cli.mod_table_menu_screen as mod_table_menu_screen
+import minemoddb.cli.modpack_table_menu_screen as modpack_table_menu_screen
 from minemoddb.cli.screen import Screen
 from minemoddb.utils import get_option
 
@@ -13,7 +14,7 @@ class TablesMenuScreen(Screen):
     def show(self) -> None:
         print('Tabelas'.upper())
 
-        options = ['Pessoas', 'Mods', 'Voltar']
+        options = ['Pessoas', 'Mods', 'Modpacks', 'Voltar']
         option = get_option(options)
         if option == 'Pessoas':
             self._program.set_screen(
@@ -21,6 +22,10 @@ class TablesMenuScreen(Screen):
         elif option == 'Mods':
             self._program.set_screen(
                 mod_table_menu_screen.ModTableMenuScreen(self._program))
+        elif option == 'Modpacks':
+            self._program.set_screen(
+                modpack_table_menu_screen
+                .ModpackTableMenuScreen(self._program))
         elif option == 'Voltar':
             self._program.set_screen(
                 main_menu_screen.MainMenuScreen(self._program))
