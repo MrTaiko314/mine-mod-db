10cb2562717a34d42d79a4faf951dd5feaa93c90
Gabriel Araujo <glucio314@gmail.com>
2022-04-28 22:51:55 -0300

    Turn Screen into an interface

--- minemoddb/cli/screen.py
+++ minemoddb/cli/screen.py
@@ -1,6 +1,9 @@
-class Screen:
-    def __init__(self) -> None:
-        pass
+from abc import ABC, abstractmethod
 
+
+class Screen(ABC):
+    """Interface base para todas as telas."""
+
+    @abstractmethod
     def show(self) -> None:
-        pass
+        raise NotImplementedError
